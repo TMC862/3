82f05a40be4d380d2596885e168f1a9d2c74eefb
TMC862 <kingrajashish43211@gmail.com>
2025-06-19 17:23:54 +0530

    Update start.py

--- devgagan/modules/start.py
+++ devgagan/modules/start.py
@@ -222,7 +222,7 @@ async def see_plan(client, callback_query):
     buttons = InlineKeyboardMarkup(
         [
             [InlineKeyboardButton("📜 See Terms", callback_data="see_terms")],
-            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/kingofpatal")],
+            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/rosy_dosy_bot")],
         ]
     )
     await callback_query.message.edit_text(plan_text, reply_markup=buttons)
@@ -240,7 +240,7 @@ async def see_terms(client, callback_query):
     buttons = InlineKeyboardMarkup(
         [
             [InlineKeyboardButton("📋 See Plans", callback_data="see_plan")],
-            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/kingofpatal")],
+            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/rosy_dosy_bot")],
         ]
     )
     await callback_query.message.edit_text(terms_text, reply_markup=buttons)
